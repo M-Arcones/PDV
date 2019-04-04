db34cf5a8001baebe5a42ddfb364d21cf68080d4
M-Arcones <43450871+M-Arcones@users.noreply.github.com>
2019-04-04 15:41:26 +0200

    Update Elegir_nave.js

--- juego/Elegir_nave.js
+++ juego/Elegir_nave.js
@@ -17,7 +17,7 @@ Game.Elegir_nave.prototype ={
 	create:function(){
 		this.seleccion;
 		/*this.game.physics.p2.gravity.y = 0;*/
-        var style = {	font: "25px Press Start",
+        var style = {	font: "25px Press Start 2P",
 						fill: "White",
 						boundsAlignH: "center",
 						boundsAlignV: "middle",
@@ -27,46 +27,46 @@ Game.Elegir_nave.prototype ={
         this.background.width = this.game.width;
 		
         this.button_nave1 = this.add.button(this.world.centerX - 370, 105, 'Boton', this.selec_nave, this, 2, 1, 0);
-		this.button_nave1.scale.setTo(1, 2);
-		this.button_nave1.nave='Nave1';
-		this.img_nave=this.add.sprite(this.world.centerX - 340, 155, 'Nave1');
-		this.img_nave.scale.setTo(2, 2);
+	this.button_nave1.scale.setTo(1, 2);
+	this.button_nave1.nave='Nave1';
+	this.img_nave=this.add.sprite(this.world.centerX - 340, 155, 'Nave1');
+	this.img_nave.scale.setTo(2, 2);
 		
         this.button_nave2 = this.add.button(this.world.centerX - 110, 105, 'Boton', this.selec_nave, this, 2, 1, 0);
-		this.button_nave2.scale.setTo(1, 2);
-		this.button_nave2.nave='Nave2';
-		this.img_nave1=this.add.sprite(this.world.centerX - 80, 155, 'Nave2');
-		this.img_nave1.scale.setTo(2, 2);
+	this.button_nave2.scale.setTo(1, 2);
+	this.button_nave2.nave='Nave2';
+	this.img_nave1=this.add.sprite(this.world.centerX - 80, 155, 'Nave2');
+	this.img_nave1.scale.setTo(2, 2);
 		
         this.button_nave3 = this.add.button(this.world.centerX + 150, 105, 'Boton', this.selec_nave, this, 2, 1, 0);
-		this.button_nave3.scale.setTo(1, 2);
-		this.button_nave3.nave='Nave3';
-		this.img_nave2=this.add.sprite(this.world.centerX + 180, 155, 'Nave3');
-		this.img_nave2.scale.setTo(2, 2);
+	this.button_nave3.scale.setTo(1, 2);
+	this.button_nave3.nave='Nave3';
+	this.img_nave2=this.add.sprite(this.world.centerX + 180, 155, 'Nave3');
+	this.img_nave2.scale.setTo(2, 2);
 		
-		switch (tipo_nave){
-			case ('Nave1'):
-				this.button_nave1.tint=1 * 0xffffff;
-				this.button_nave2.tint=0.4 * 0xffffff;
-				this.button_nave3.tint=0.4 * 0xffffff;
-				break;
-			case ('Nave2'):
-				this.button_nave2.tint=1 * 0xffffff;
-				this.button_nave1.tint=0.4 * 0xffffff;
-				this.button_nave3.tint=0.4 * 0xffffff;
-				break;
-			case ('Nave3'):
-				this.button_nave3.tint=1 * 0xffffff;
-				this.button_nave1.tint=0.4 * 0xffffff;
-				this.button_nave2.tint=0.4 * 0xffffff;
-				break;
-		}
-		this.button_volver = this.add.button(this.world.centerX - 160, 405, 'Boton', this.click_button, this, 2, 1, 0);
-		this.button_volver.scale.setTo(1.4, 0.9);
-		this.button_volver.stage='Settings';
-		this.text2=this.game.add.text(0, 0, "Volver",style);
-		this.text2.font = 'Press Start';
-		this.text2.setTextBounds(0, 400, this.game.world.width,100);
+	switch (tipo_nave){
+		case ('Nave1'):
+			this.button_nave1.tint=1 * 0xffffff;
+			this.button_nave2.tint=0.4 * 0xffffff;
+			this.button_nave3.tint=0.4 * 0xffffff;
+			break;
+		case ('Nave2'):
+			this.button_nave2.tint=1 * 0xffffff;
+			this.button_nave1.tint=0.4 * 0xffffff;
+			this.button_nave3.tint=0.4 * 0xffffff;
+			break;
+		case ('Nave3'):
+			this.button_nave3.tint=1 * 0xffffff;
+			this.button_nave1.tint=0.4 * 0xffffff;
+			this.button_nave2.tint=0.4 * 0xffffff;
+			break;
+	}
+	this.button_volver = this.add.button(this.world.centerX - 160, 405, 'Boton', this.click_button, this, 2, 1, 0);
+	this.button_volver.scale.setTo(1.4, 0.9);
+	this.button_volver.stage='Settings';
+	this.text2=this.game.add.text(0, 0, "Volver",style);
+	this.text2.font = 'Press Start';
+	this.text2.setTextBounds(0, 400, this.game.world.width,100);
 	},
 	update:function(){
 		this.scale.pageAlignHorizontally = true;
